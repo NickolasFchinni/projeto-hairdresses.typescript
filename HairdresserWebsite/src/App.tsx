19c74d2e00b44c0249c9ec1fd7d077a9e12be9cc
Nickolas Faquini <101142440+Nickolas2019@users.noreply.github.com>
2024-01-15 19:20:26 -0300

    form

--- HairdresserWebsite/src/App.tsx
+++ HairdresserWebsite/src/App.tsx
@@ -4,6 +4,7 @@ import Benefits from '@/scenes/benefits'
 import { useEffect, useState } from 'react';
 import SelectedPage from './shared/types';
 import OurClasses from '@/scenes/ourclasses'
+import ContactUs from './scenes/contactUs';
 
 
 function App() {
@@ -35,6 +36,7 @@ function App() {
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
     </div>
   )
 }
